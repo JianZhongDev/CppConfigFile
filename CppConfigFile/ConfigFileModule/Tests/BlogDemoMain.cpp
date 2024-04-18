107de1f6e4b429b60ffa814d761e4ca466413c84
Jian Zhong <jian.zhong.dev@gmail.com>
2024-04-18 13:22:00 -0700

    add demo code for vision tech insights

--- CppConfigFile/ConfigFileModule/Tests/BlogDemoMain.cpp
+++ CppConfigFile/ConfigFileModule/Tests/BlogDemoMain.cpp
@@ -1,18 +1,20 @@
 /* Code Description
-* FILE: DemoMain.cpp
-* DESCRIPTON: demo main function for generic hashmap
+* FILE: BlogDemoMain.cpp
+* DESCRIPTON: demo main function for generic hashmap for blog post
 * NOTE:
 *
 * CREATE: 2024-04-06
 * LAST UPDATE:
 */
 
+
 #include<iostream>
 #include<string>
 #include<vector>
 #include<unordered_map>
 
-//#define MAIN // enable or disable main method in this file
+
+#define MAIN // enable or disable main method in this file
 
 #ifdef MAIN
 
@@ -25,19 +27,12 @@ void main() {
 	GenHashMapIOTxt test_io_txt;
 
 	// initialize generic hash map
-	test_genhashmap["int01"] = new IntEntry(-1);
-	test_genhashmap["int02"] = new IntEntry(-2);
-	test_genhashmap["uint01"] = new UintEntry(1);
-	test_genhashmap["uint02"] = new UintEntry(2);
-	test_genhashmap["float01"] = new FloatEntry(0.1);
-	test_genhashmap["float02"] = new FloatEntry(0.2);
-	test_genhashmap["ulonglong01"] = new ULongLongEntry(1000);
-	test_genhashmap["ulonglong02"] = new ULongLongEntry(2000);
-	test_genhashmap["string01"] = new StringEntry("Hello world!");
-	test_genhashmap["string02"] = new StringEntry("Welcome to Vision Tech Insights!");
-	test_genhashmap["vector_float01"] = new VectorFloatEntry({ 0.1, 0.2, 0.3, 0.4, 0.5 });
-	test_genhashmap["vector_float02"] = new VectorFloatEntry({ 0.6, 0.7, 0.8, 0.9, 1.0, 1.1});
+	test_genhashmap["string_val"] = new StringEntry("Welcome to Vision Tech Insights!");
+	test_genhashmap["vector_float_val"] = new VectorFloatEntry({ 0.1, 0.2, 0.3, 0.4, 0.5 });
 
+	std::cout << "====== Initialize gen hashmap ======" << std::endl << std::endl;
+
+	// print all the values in the generic hash map
 	std::cout << "--- genhashmap values START ---" << std::endl;
 	for (const auto& key_val_pair : test_genhashmap) {
 		std::string cur_name_str = key_val_pair.first;
@@ -49,20 +44,25 @@ void main() {
 	}
 	std::cout << "--- genhashmap values END ---" << std::endl;
 	std::cout << std::endl;
-	
+
+
+	// save generic hash map into a text file
+	std::cout << "====== Save gen hashmap to text file ======" << std::endl << std::endl;
+
 	// save to txt configuration file
 	test_io_txt.save_to_file(
 		test_genhashmap,
-		"test.txt",
+		"test_config_file.txt",
 		std::ios_base::out,
-		"This is a test configuration file."
+		"This is the test configuration file for Vision Tech Insights blog."
 	);
 
 
+	// save generic hash map into a text file
+	std::cout << "====== Set values in gen hashmap ======" << std::endl << std::endl;
 	// try to change some of the values
-	((FloatEntry*)test_genhashmap["float01"])->set(-0.1);
-	((ULongLongEntry*)test_genhashmap["ulonglong02"])->set(1500);
-	((StringEntry*)test_genhashmap["string02"])->set(std::string("some random string"));
+	((StringEntry*)test_genhashmap["string_val"])->set(std::string("Hello world!"));
+	((VectorFloatEntry*)test_genhashmap["vector_float_val"])->set(std::vector<float>{-0.1, -0.2, -0.3});
 
 	std::cout << "--- genhashmap values START ---" << std::endl;
 	for (const auto& key_val_pair : test_genhashmap) {
@@ -76,30 +76,34 @@ void main() {
 	std::cout << "--- genhashmap values END ---" << std::endl;
 	std::cout << std::endl;
 
-	// try to get some of the values
-	std::vector<float> tmp_float_vec;
-	((VectorFloatEntry*)test_genhashmap["vector_float01"])->get(&tmp_float_vec);
+	// try to get value from genhashmap
+	std::cout << "====== Get values from gen hashmap ======" << std::endl << std::endl;
 	std::string tmp_string;
-	((StringEntry*)test_genhashmap["string02"])->get(&tmp_string);
-	unsigned long long tmp_ulonglong;
-	((ULongLongEntry*)test_genhashmap["ulonglong01"])->get(&tmp_ulonglong);
-
+	((StringEntry*)test_genhashmap["string_val"])->get(&tmp_string);
+	std::vector<float> tmp_float_vec;
+	((VectorFloatEntry*)test_genhashmap["vector_float_val"])->get(&tmp_float_vec);
 
 	std::cout << "--- get values START ---" << std::endl;
 	std::cout << "tmp_float_vec = {";
-	for (const auto& val : tmp_float_vec) {
-		std::cout << val << ", ";
+	for (int i_val = 0; i_val < tmp_float_vec.size(); i_val++) {
+		std::cout << tmp_float_vec[i_val];
+		if (i_val < tmp_float_vec.size() - 1) {
+			std::cout << ", ";
+		}
 	}
 	std::cout << "}" << std::endl;
 	std::cout << "tmp_string = " << tmp_string << std::endl;
-	std::cout << "tmp_ulonglong = " << tmp_ulonglong << std::endl;
 	std::cout << "--- get values END ---" << std::endl;
 	std::cout << std::endl;
+
+
+	// load generic hash map from the text file
+	std::cout << "====== Load gen hashmap from text file ======" << std::endl << std::endl;
 
 	// update values according to txt configuration file
 	test_io_txt.update_from_file(
 		test_genhashmap,
-		"test.txt",
+		"test_config_file.txt",
 		std::ios_base::in
 	);
 
